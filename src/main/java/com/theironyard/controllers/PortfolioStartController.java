5cd35287ef301e6cef8825a1c13a644935d31983
Jennifer Chang <jennichang02@gmail.com>
2016-09-24 18:20:31 -0400

    working through being logged in

--- src/main/java/com/theironyard/controllers/PortfolioStartController.java
+++ src/main/java/com/theironyard/controllers/PortfolioStartController.java
@@ -6,6 +6,7 @@ import com.theironyard.utilities.PasswordStorage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -20,9 +21,15 @@ public class PortfolioStartController {
 
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String home() {
+    public String home(HttpSession session, Model model) {
+        String email = (String) session.getAttribute("email");
+        User user = users.findFirstByEmail(email);
+        if (user != null) {
+            model.addAttribute("user", user);
+        }
         return "home";
     }
+
 
     @RequestMapping(path = "/login", method = RequestMethod.POST)
     public User login(String email, String password, HttpSession session, HttpServletResponse response) throws Exception {
@@ -46,6 +53,12 @@ public class PortfolioStartController {
     public String signupPost(String firstName, String lastName, String email, String password) {
         User user = new User(firstName, lastName, email, password);
         users.save(user);
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/logout", method = RequestMethod.POST)
+    public String logout(HttpSession session) {
+        session.invalidate();
         return "redirect:/";
     }
 
