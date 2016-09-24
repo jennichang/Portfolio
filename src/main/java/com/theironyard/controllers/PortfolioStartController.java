8ec3eb7a7d4f439df21a1c1eb4f87d4d2385dc93
Jennifer Chang <jennichang02@gmail.com>
2016-09-24 16:29:46 -0400

    why is this happening

--- src/main/java/com/theironyard/controllers/PortfolioStartController.java
+++ src/main/java/com/theironyard/controllers/PortfolioStartController.java
@@ -1,5 +1,7 @@
 package com.theironyard.controllers;
 
+import com.theironyard.entities.User;
+import com.theironyard.services.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
@@ -12,10 +14,27 @@ import javax.servlet.http.HttpSession;
 @EnableAutoConfiguration
 public class PortfolioStartController {
 
+    @Autowired
+    UserRepository users;
+
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String home() {
         return "home";
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public User login(String username, String password, HttpSession session, HttpServletResponse response) throws Exception {
+        User user = users.findFirstByName(username);
+        if (user == null) {
+            user = new User(username, PasswordStorage.createHash(password));
+            users.save(user);
+        } else if (!PasswordStorage.verifyPassword(password, user.getPassword())) {
+            throw new Exception("Wrong password");
+        }
+        session.setAttribute("username", username);
+        response.sendRedirect("/");
+        return user;
     }
 
 
