2d4d5dc3cf5fc7ff3d2a73a1ddf116a1a08c5056
Jennifer Chang <jennichang02@gmail.com>
2016-09-25 09:47:50 -0400

    new login fixed

--- src/main/java/com/theironyard/controllers/PortfolioStartController.java
+++ src/main/java/com/theironyard/controllers/PortfolioStartController.java
@@ -23,14 +23,45 @@ public class PortfolioStartController {
 
     @RequestMapping(path = "/", method = RequestMethod.GET)
     public String home(HttpSession session, Model model) {
-//        String email = (String) session.getAttribute("email");
-//        User user = users.findFirstByEmail(email);
-//        if (user != null) {
-//            model.addAttribute("user", user);
-//        } user = new User("Visitor", "no user", "no user", "no user");
-//        model.addAttribute("user", user);
+        String email = (String) session.getAttribute("email");
+        User user = users.findFirstByEmail(email);
+        model.addAttribute("user", user);
         return "home";
     }
+
+    @RequestMapping(path = "/login", method = RequestMethod.POST)
+    public String login(String email, String password, HttpSession session, HttpServletResponse response) throws Exception {
+        User user = users.findFirstByEmail(email);
+        if (user == null) {
+            response.sendRedirect("/signup");
+        } else if (!PasswordStorage.verifyPassword(password, user.getPassword())) {
+            throw new Exception("Wrong password");
+        }
+        session.setAttribute("email", email);
+        return "redirect:/";
+    }
+
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String loginGet() {
+        return "login";
+    }
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////
+
+
+
+
+
+
 
     @RequestMapping(path = "/user", method = RequestMethod.GET)
     public String user(HttpSession session, Model model) {
@@ -49,17 +80,16 @@ public class PortfolioStartController {
     }
 
 
-    @RequestMapping(path = "/login", method = RequestMethod.POST)
-    public String login(String email, String password, HttpSession session, HttpServletResponse response) throws Exception {
-        User user = users.findFirstByEmail(email);
-        if (user == null) {
-            response.sendRedirect("/signup");
-        } else if (!PasswordStorage.verifyPassword(password, user.getPassword())) {
-            throw new Exception("Wrong password");
-        }
-        session.setAttribute("username", email);
-        return "redirect:/user";
-    }
+
+
+
+
+
+
+
+
+
+
 
     @RequestMapping(path = "/signup", method = RequestMethod.GET)
     public String signup() {
